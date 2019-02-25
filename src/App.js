b0695011ab77fcff3e3621dcffa7d8dc5ec02d13
Preet Dha <manpreetdha+github@gmail.com>
2019-02-24 16:21:55 -0800

    Add semantic ui to project, add button and title with minor styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import index from './index.css';
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui center aligned container">
+        <div className="title">Random Quote Generator</div>
+        <div className="ui section divider" />
+        <button className="massive blue ui button">Get a Quote!</button>
+      </div>
+    );
   }
 }
 
