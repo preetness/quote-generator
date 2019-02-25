fc2f3c3b100ef2fbcac8a51afe55e9d3d80622df
Preet Dha <manpreetdha+github@gmail.com>
2019-02-25 10:09:59 -0800

    Change CSS: fix font and background colors, and title sizing

--- src/QuoteDetails.js
+++ src/QuoteDetails.js
@@ -5,17 +5,15 @@ class QuoteDetails extends React.Component {
     const { quote, author } = this.props;
     return (
       quote !== '' && (
-        <div>
-          <div className="ui center aligned card container">
-            <div className="content">
-              <div className="center aligned description">
-                <p>
-                  <i className="fas fa-quote-left" />
-                  {quote}
-                  <i className="fas fa-quote-right" />
-                </p>
-                <div className="center aligned header">{author}</div>
-              </div>
+        <div className="ui center aligned card container">
+          <div className="content">
+            <div className="center aligned description">
+              <p>
+                <i className="fas fa-quote-left" />
+                {quote}
+                <i className="fas fa-quote-right" />
+              </p>
+              <div className="center aligned header">{author}</div>
             </div>
           </div>
         </div>
