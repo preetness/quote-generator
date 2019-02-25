6b635c79f56709294a4f1c709f4772eadd27c602
Preet Dha <manpreetdha+github@gmail.com>
2019-02-24 17:09:46 -0800

    Successfully make api call to Forismatic API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import index from './index.css';
+import Axios from 'axios';
 
 class App extends React.Component {
-  onButtonClick = () => {
-    console.log('clicked');
+  getQuote = async () => {
+    try {
+      const response = await Axios.get(
+        'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   render() {
     return (
       <div className="ui center aligned container">
         <div className="title">Random Quote Generator</div>
         <div className="ui section divider" />
-        <button className="massive blue ui button" onClick={this.onButtonClick}>
+        <button className="massive blue ui button" onClick={this.getQuote}>
           Get a Quote!
         </button>
       </div>
